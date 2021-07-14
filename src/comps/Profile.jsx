430568712cd9615733c7b58b15763a802f6b13e1
Vidmis <v.gutauskas90@gmail.com>
2021-07-15 00:27:57 +0300

    Profile card color update

--- src/comps/Profile.jsx
+++ src/comps/Profile.jsx
@@ -4,14 +4,14 @@ const Profile = () => {
   return (
     <>
       <div className='card-content'>
-        <div className='bg-palette-sunset overflow-hidden w-2/5 mt-20 m-auto items-center rounded-md'>
+        <div className='bg-palette-cloud shadow-lg overflow-hidden w-2/5 mt-20 m-auto items-center rounded-md'>
           <form className='card flex flex-col m-auto items-center'>
             <img
               src={pepe}
               alt='profile_photo'
               className='w-48 h-48 rounded-md mt-5'
             />
-            <h3 className='profile-name my-3 text-xl font-medium text-palette-cloud'>Bette Random</h3>
+            <h3 className='profile-name my-3 text-xl font-medium text-gray-600'>Bette Random</h3>
             <input
               className='transition duration-150 ease-in-out font-medium focus:shadow-md focus:ring-2 focus:ring-palette-teal text-palette-moon rounded-md no-underline w-3/5 min-w-32 text-center py-1 px-2 focus:outline-none mt-4 focus:text-gray-600'
               type='text'
